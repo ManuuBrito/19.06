6741a617fa3d3483e7af31397fcbaec42c7bfcd4
ManuuBrito <129909959+ManuuBrito@users.noreply.github.com>
2023-06-21 21:50:09 -0300

    Add files via upload

---
+++ java script/exercicio.js
@@ -0,0 +1,59 @@
+let pagamento = "débito";
+
+//if(pagamento === "débito"){
+    //console.log("recebe 10% de desconto");
+//}else if() 
+//if(pagamento === "dinheiro" || pagamento === "pix"){
+    //console.log("15% de desconto");
+//}else if(pagamento === "2x"){
+    //console.log("preço normal de etiqueta sem juros")
+//}else{
+    //console.log("preço normal de etiqueta mais juros de 10%")
+//}
+
+/*switch (pagamento) {
+    case "débito":
+        console.log("receba 10% de desconto");
+        break;
+        case "dinheiro":
+            console.log("recebe 15% de desconto")
+        case "pix":
+            console.log("recebe 15% de desconto");
+            break;
+            case "2x":
+                console.log("preço normal de etiqueta sem juros");
+                break;
+                default:
+                    console.log("preço normal de etiquetas mais juros de 10%");
+                    break;
+}*/
+//let carro = "renegade";
+
+//switch (carro){
+    //case "renegade":
+        //console.log("jeep"):
+        //break;
+        //case "c4 cactus":
+            //console.log("citroen");
+            //break;
+            //case "hb20":
+            //console.log("hyundai");
+            //break;
+            //case "kicks":
+            //console.log("nissan");
+            //break;
+            //case "focus":
+            //console.log("ford");
+            //break;
+            //case "tracker":
+            //console.log("chevrolet");
+            //break;
+            //case "corola":
+            //console.log("toyota");
+            //break;
+            //default:
+                //console.log("escolha alguma marca")
+//}
+for (let i = 0; i < 11; i++) {
+    console.log("O resultado de 5 x é ", i, "=", 5 * i);
+}
