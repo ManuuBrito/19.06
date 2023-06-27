00576d8c7ce5acdcd9e082792c317488542a954d
ManuuBrito <129909959+ManuuBrito@users.noreply.github.com>
2023-06-26 21:54:53 -0300

    aula estrambólica
    aula infinita plmrd

--- java script/exercicio.js
+++ java script/exercicio.js
@@ -54,6 +54,35 @@ let pagamento = "débito";
             //default:
                 //console.log("escolha alguma marca")
 //}
-for (let i = 0; i < 11; i++) {
+/*for (let i = 0; i < 11; i++) {
     console.log("O resultado de 5 x é ", i, "=", 5 * i);
+}*/
+let i = 0;
+while (i <=10) {
+    console.log( "O resultado de 5 x", i,"=", 5 * i);
+    i++
 }
+
+let num1 = 5;
+let num2 = 10;
+
+soma = num1+num2;
+
+console.log(soma);
+function soma(x, y){
+    const resultado = x + y;
+    return resultado;
+}
+console.log(soma(20 , 30));
+console.log(soma(15 , 55));
+
+function saudacao(){
+    return "Boa noite pessoal";
+}
+console.log(saudacao());
+
+function saud(nome){
+    return `Boa noite ${nome}`;
+}
+
+console.log(saud("Maria"));
---
+++ java script/exercicios2.js
@@ -0,0 +1,60 @@
+//let i = 0;
+//while (i <=10) {
+    //console.log( "O resultado de 5 x", i,"=", 5 * i);
+  //  i++
+//}
+
+//let num1 = 5;
+//let num2 = 10;
+
+//let soma = num1+num2;
+
+//console.log(soma);
+//function soma(x, y){
+  //  const resultado = x + y;
+   // return resultado;
+//}
+//console.log(soma(20 , 30));
+//console.log(soma(15 , 55));
+
+//function saudacao(){
+   // return "Boa noite pessoal";
+//}
+//console.log(saudacao());
+
+//function saud(nome){
+  //  return `Boa noite ${nome}`;
+//}
+
+//console.log(saud("Maria"));
+
+//let altura2 = 1.62;
+//let peso2 = 88;
+
+//let imc2 = peso2 / (altura2 * altura2);
+
+//console.log(imc2);
+function imc(peso, altura){
+    let resultado = peso / (altura * altura);
+    if (resultado <= 24.9) {
+        return "Peso normal"
+    }else if(resultado>=25 &&resultado <=29.9){
+        return "Pessoa com sobrepeso"
+    }else if(resultado>=29.9){
+        return "Obesidade grau I"
+    }else if(resultado>=34.9){
+        return "Obesidade grau iI"
+    }else if(resultado>=40){
+        return "Obesidade grau III"
+    }
+}
+console.log(img(80, 1.80).toFixed(2));
+console.log(img(70, 1.50).toFixed(2));
+console.log(img(90, 2.0).toFixed(2));
+console.log(img(50, 1.60).toFixed(2));
+
+let i = 0;
+while (i <= 10) {
+    console.log(5 * i)
+    i++
+}
